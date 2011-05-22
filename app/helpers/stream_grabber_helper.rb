5effa3af1b33c4cf3eba9a5791dcaf3a2f720908
Matt House <matt@eightbitraptor.com>
2011-05-22 15:24:55 +0100

    configurable numbers, classes and id's in a helper

--- app/helpers/stream_grabber_helper.rb
+++ app/helpers/stream_grabber_helper.rb
@@ -1,8 +1,14 @@
 module StreamGrabberHelper
-  def generate_stream_list
-    data = StreamGrabber.grab(5).inject([]){ |memo,acc| memo << content_tag(:li,raw(acc[1])) }
-    content_tag :ul, :class => "activity-stream" do
-      data.join("\n")
+  def generate_stream_list(num=15, *args)
+    if args.present?
+      css_class = args.first[:class]
+      css_id = args.first[:id]
+    end
+
+    data = StreamGrabber.grab(num).inject([]){ |memo,acc| memo << content_tag(:li,raw(acc[1])) }
+
+    content_tag :ol, {:class => css_class || 'activity_list', :id => css_id || 'stream_grabber'} do 
+      raw(data.join("\n"))
     end
   end
 end
