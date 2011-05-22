e0b93f8d99eb29605e7c930ea35953c2d14a92f1
Matt House <matt@eightbitraptor.com>
2011-05-22 15:24:14 +0100

    what happens when we ask for mre than we have.

--- test/stream_grabber_test.rb
+++ test/stream_grabber_test.rb
@@ -12,6 +12,17 @@ class StreamGrabberTest < Test::Unit::TestCase
     end
   end
 
+  context "Passing in a ridiculously high number" do
+    setup do
+      @results = StreamGrabber.grab(53)
+    end
+
+    should "only show what it can" do
+      timestamps = @results.first(5).map{ |r| r[0] }
+      assert_equal expected_timestamps, timestamps
+    end
+  end
+
   def expected_timestamps
     [1287353685, 1287351412, 1287342471, 1287340078, 1287328637]
   end
